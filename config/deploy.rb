7668907f11f591b6db664437f2f45cd429cf1618
torunakano <torunakano@torunoMacBook-Pro.local>
2017-07-22 12:50:55 +0900

    Change files

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+# config valid only for current version of Capistrano
+lock "3.8.2"
+
+set :application, 'piece-of-cake'
+set :repo_url,  'git@github.com:torunakano/piece-of-cake.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['/Users/torunakano/.ssh/toru_key.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+set :linked_files, %w{ config/secrets.yml }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
