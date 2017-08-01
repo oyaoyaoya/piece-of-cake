c1b449670a1f9c485c7f3d01be11f684da9cdd34
torunakano <torunakano@torunoMacBook-Pro.local>
2017-08-01 22:25:03 +0900

    Add user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,23 @@ class UsersController < ApplicationController
 
   def show
      @user = current_user
-     @notes = current_user.notes
-    
+     @notes = current_user.notes.order("created_at DESC")
    end
 
   def edit
+    @user = current_user
   end
 
   def update
+     current_user.update(user_params)
+       redirect_to user_path
+    # else
+    #   render :show_url
+    # end
   end
 
-
-
+  private
+  def user_params
+    params.require(:user).permit(:headerimage, :image, :nickname, :introduction)
+  end
 end
