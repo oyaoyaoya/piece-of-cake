d2aa30c5be6e185fa02c8ce858676a86f99a8d92
torunakano <torunakano@torunoMacBook-Pro.local>
2017-08-09 19:49:03 +0900

    Add changes to /notes/new page
    Add js files to show uploaded image

---
+++ app/assets/javascripts/image-show.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(".insert-icon").on('change', function(){
+     var fileprop = $(this).prop('files')[0],
+         find_img = $(this).parent().find('img'),
+         filereader = new FileReader(),
+         view_box = $(this).parent('.image-insert');
+
+    if(find_img.length){
+       find_img.nextAll().remove();
+       find_img.remove();
+    }
+
+    var img = '<div class="img_view"><img alt="" class="img"></div>';
+
+    $(".image-insert").append(img);
+
+    filereader.onload = function() {
+      view_box.find('img').attr('src', filereader.result);
+      img_del(view_box);
+    }
+    filereader.readAsDataURL(fileprop);
+  });
+});
